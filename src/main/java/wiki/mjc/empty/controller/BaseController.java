ea02a87a3b9ba482855d13bec6aa8a98f0eee948
Jianchao Meng <jianchaomeng@sohu-inc.com>
2017-04-06 19:19:03 +0800

    Revert "add springMVC & ElasticSearch"
    This reverts commit e0ee09b86cfd7ee1c730f8c1c54456773f817677.

--- src/main/java/wiki/mjc/empty/controller/BaseController.java
+++ src/main/java/wiki/mjc/empty/controller/BaseController.java
@@ -3,38 +3,23 @@ package wiki.mjc.empty.controller;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import wiki.mjc.empty.utils.ElasticSearchUtil;
 
 @Controller
 public class BaseController {
 
-	protected Logger logger = LogManager.getLogger("test");
-	
-	@Autowired
-	ElasticSearchUtil elasticSearchUtil;
-			
 	@RequestMapping("/")
 	public String HelloWorld() {
-		logger.info("helloworld");
-		//elasticSearchUtil.init();
-		
 		return "helloworld";
 	}
 	
-	@RequestMapping("/put")
+	@RequestMapping("/json")
 	@ResponseBody
-	public Map<String, Object> json(@RequestParam String value) {
+	public Map<String, Object> json() {
 		Map<String, Object> ret = new HashMap<String, Object>();
 		ret.put("result", "hello world");
-		elasticSearchUtil.ttt(value);
 		return ret;
 	}
 }
